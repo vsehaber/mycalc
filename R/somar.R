b2182e708188dd7c3ba5e39f02b83c96d935967c
Vanessa F. Sehaber <vsehaber@gmail.com>
2024-10-25 10:12:29 -0300

    Adicionei espaço na função somar

--- R/somar.R
+++ R/somar.R
@@ -3,11 +3,14 @@
 #' Esta função serve para somar dois números reais a e b.
 #'
 #'
+#'
 #' @param a Um número
 #' @param b Outro número
 #'
+#'
 #' @examples
 #' somar(2,2)
+#'
 #'
 #' @export
 somar <- function(a,b){
